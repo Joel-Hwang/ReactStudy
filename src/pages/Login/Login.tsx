abd5c0bbfe93cf5ed22068c1abf2fe946c7025e8
Joel Hwang <joel.c.hwang@gmail.com>
2023-03-25 21:27:22 +0900

    login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -12,47 +12,50 @@ interface User {
 
 const Login: React.FC<LoginProps> = ({ onLogin }) => {
   const [user, setUser] = useState<User>({ username: '', password: '' });
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-  };
-
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setUser((prevUser) => ({ ...prevUser, [name]: value }));
-  };
-
-  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault();
     // 로그인 처리 코드
     console.log(user);
     onLogin();
   };
 
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    console.log("name :" + name, "value : " + value);
+    setUser((prevUser) => ({ ...prevUser, [name]: value }));
+  };
+
   return (
     <div className='login-container'>
       <h2>PLM</h2>
-
-      <Paper className='input-area' variant="outlined">
-        <div>
-          <TextField id="outlined-basic" label="ID" variant="standard" />
-        </div>
-        <br/>
+      <div className='input-area'>
         <div>
           <TextField
-            id="standard-password-input"
+            id="username"
+            name="username"
+            label="ID"
+            variant="standard"
+            onChange={handleInputChange}
+            value={user.username}
+          />
+        </div>
+        <br />
+        <div>
+          <TextField
+            id="password"
+            name="password"
             label="Password"
             type="password"
             autoComplete="current-password"
             variant="standard"
+            onChange={handleInputChange}
+            value={user.password}
           />
         </div>
-      </Paper>
-      <div className='button-area'>
-        <Button variant="contained">Log in</Button>
+      </div>
+      <div className='login-button'>
+        <Button variant="contained" onClick={handleClick}>Log in</Button>
       </div>
 
 
