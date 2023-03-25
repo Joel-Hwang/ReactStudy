3b6c077b6bd477fef8e863a880f3f2c24a1257e6
Joel Hwang <joel.c.hwang@gmail.com>
2023-03-25 17:52:42 +0900

    ggg

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,39 +1,61 @@
 import React, { useState } from 'react';
+import { TextField, Paper, Button } from '@mui/material';
 import './Login.css'
 interface LoginProps {
-    onLogin:() => void;
+  onLogin: () => void;
 }
 
 interface User {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
-const Login: React.FC<LoginProps> = ({onLogin}) => {
-    const [user, setUser] = useState<User>({ username: '', password: '' });
+const Login: React.FC<LoginProps> = ({ onLogin }) => {
+  const [user, setUser] = useState<User>({ username: '', password: '' });
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const { name, value } = event.target;
-      setUser((prevUser) => ({ ...prevUser, [name]: value }));
-    };
-  
-    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      // 로그인 처리 코드
-      console.log(user);
-      onLogin();
-    };
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUser((prevUser) => ({ ...prevUser, [name]: value }));
+  };
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // 로그인 처리 코드
+    console.log(user);
+    onLogin();
+  };
 
   return (
-    <div className="login-container">
-      <form id="login-form" onSubmit={handleFormSubmit}>
-        <h2>PLM</h2>
-        <label htmlFor="username">아이디:</label>
-        <input type="text" id="username" name="username" required value={user.username} onChange={handleInputChange} />
-        <label htmlFor="password">비밀번호:</label>
-        <input type="password" id="password" name="password" required value={user.password} onChange={handleInputChange} />
-        <button type="submit">로그인</button>
-      </form>
+    <div className='login-container'>
+      <h2>PLM</h2>
+
+      <Paper className='input-area' variant="outlined">
+        <div>
+          <TextField id="outlined-basic" label="ID" variant="standard" />
+        </div>
+        <br/>
+        <div>
+          <TextField
+            id="standard-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            variant="standard"
+          />
+        </div>
+      </Paper>
+      <div className='button-area'>
+        <Button variant="contained">Log in</Button>
+      </div>
+
+
     </div>
   );
 };
